c1eae1320d5002786c746e0cd876a0fcfaf252ae
Lewis Lancaster <prixelstudios@gmail.com>
2015-06-22 22:29:41 +0100

    Worked on the account a little bit, and facepunch too

--- src/Account.php
+++ src/Account.php
@@ -1,9 +1,30 @@
 <?php
 namespace Cornpunch;
 
-class Account {
-    
-    public function __construct() {
+use Cornpunch\Libraries\Facepunch as FacepunchWrapper;
 
+class Account {
+    protected $facepunch;
+    protected $account;
+    public function __construct()
+    {
+        $this->facepunch = new FacepunchWrapper;
+    }
+    public function bootstrapAccount( $userid ){
+        //We first check to see if this account is a valid facepunch account
+        if(!$this->facepunch->isUserValid())
+            return false;
+        //Set the ID
+        $this->account = $userid;
+    }
+    public function cacheFacepunchData()
+    {
+        //Set the userid
+        $this->facepunch->setUserID($this->account);
+        //If the user valid?
+        /**
+         * if( $this->facepunch->isUserValid() )
+         *      $database->updateUserData( $this->facepunch->getUserInformation( true ) );
+         */
     }
 }
