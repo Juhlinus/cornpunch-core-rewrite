0b558b44fb7caea55d89ee59e740b37e78f44729
Juhlinus <dragonbaberiven@gmail.com>
2015-06-22 22:06:14 +0200

    Play nice with the other kids, use comments.

--- src/Database.php
+++ src/Database.php
@@ -7,24 +7,39 @@ class Database {
     
     protected $capsule;
 
+    /**
+     * Creates new instance for Capsule object
+     * and calls config function
+     */
     public function __construct() {
         $this->capsule = new Capsule;
-        $this->config();
+        echo ($this->config()) ? "Config done" : "Config error";
     }
 
+    /**
+     * Config for the Connection
+     * @return bool
+     */
     public function config() {
-        $this->capsule->addConnection([
-            'driver'    => 'mysql',
-            'host'      => 'localhost',
-            'database'  => 'cornpunch',
-            'username'  => 'root',
-            'password'  => 'password',
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-        ]);
+        if ($this->capsule->addConnection([
+                'driver'    => 'mysql',
+                'host'      => 'localhost',
+                'database'  => 'cornpunch',
+                'username'  => 'root',
+                'password'  => 'password',
+                'charset'   => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix'    => '',
+        ]))
+            return true;
+        else
+            return false;
     }
 
+    /**
+     * Returns Active Users by ID
+     * @return array
+     */
     public function activeUsers() {
         $userids = Capsule::table('activeusers')->select('userid');
 
