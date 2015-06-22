c1c0826c33bafa4516ea912c65f80ed86b706842
Juhlinus <dragonbaberiven@gmail.com>
2015-06-22 21:57:53 +0200

    More for Database Class

--- src/Database.php
+++ src/Database.php
@@ -1,9 +1,33 @@
 <?php
 namespace Cornpunch;
 
+use Illuminate\Database\Capsule\Manager as Capsule;
+
 class Database {
     
-    public function __construct() {
+    protected $capsule;
 
+    public function __construct() {
+        $this->capsule = new Capsule;
+        $this->config();
+    }
+
+    public function config() {
+        $this->capsule->addConnection([
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'cornpunch',
+            'username'  => 'root',
+            'password'  => 'password',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]);
+    }
+
+    public function activeUsers() {
+        $userids = Capsule::table('activeusers')->select('userid');
+
+        return $userids;
     }
 }
